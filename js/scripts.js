0bd3733342efe683712e6354cc8cd0c8bc2f7e0f
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-08 14:47:42 +0300

    Add function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
-var sideOne = document.getElementById('sideOne').value;//parseFloat(prompt("enter length of the first side of triangle"));
-var sideTwo = document.getElementById('sideTwo').value;//parseFloat(prompt("enter length of the second side of triangle"));
-var sideThree = document.getElementById('sideThree').value;//parseFloat(prompt("enter length of the third side of triangle"));
+function triangle(){
+	var sideOne = document.getElementById('sideOne').value;//parseFloat(prompt("enter length of the first side of triangle"));
+	var sideTwo = document.getElementById('sideTwo').value;//parseFloat(prompt("enter length of the second side of triangle"));
+	var sideThree = document.getElementById('sideThree').value;//parseFloat(prompt("enter length of the third side of triangle"));
 
 
-if(sideOne === sideTwo && sideTwo === sideThree){
+	if(sideOne === sideTwo && sideTwo === sideThree){
 		alert("You have an equilateral triangle");
-}
-else if(sideOne === sideTwo || sideTwo === sideThree || sideOne===sideThree){
+	}
+	else if(sideOne === sideTwo || sideTwo === sideThree || sideOne===sideThree){
 	alert("You an isosceles triangle");
-}
-else if (sideOne + sideTwo < sideThree || sideTwo +sideThree < sideOne || sideOne + sideThree < sideTwo) {
+	}
+	else if (sideOne + sideTwo < sideThree || sideTwo +sideThree < sideOne || sideOne + sideThree < sideTwo) {
 	alert("no triangle formed");
-}
-else{
+	}
+	else{
 	alert("you have an scalene triangle");
+	}
 }
