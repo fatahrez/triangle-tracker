f40c1a6ca8c73bfa5203c473f982d81500e2554b
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-10 10:37:30 +0300

    Add comments scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,12 @@ function triangle(){
 	var B = parseFloat(document.getElementById('sideTwo').value);
 	var C = parseFloat(document.getElementById('sideThree').value);
 
+	/*make variables to count for triangles which will not form*/
 	var AB = A + B;
 	var AC = A + C;
 	var BC = B + C;
 
-
+	/*Add if ... else statements to calculate all possible conditions*/
 	 if ((AB < C) || (AC < B) || (BC < A)) {
 		 alert("There is no triangle formed");
 	}
